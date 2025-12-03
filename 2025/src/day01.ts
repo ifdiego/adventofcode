0e31c374f28c94679b5a52072ee58cc08cc55d3f
Diego Alves <ifdiego@users.noreply.github.com>
2025-12-02 21:52:44 -0300

    part two of day 01

--- 2025/src/day01.ts
+++ 2025/src/day01.ts
@@ -6,14 +6,6 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function cycle(operation, a, b) {
-    const mod = 100;
-    let result;
-    if (operation === "R") result = a + b;
-    else if (operation === "L") result = a - b;
-    return ((result % mod) + mod) % mod;
-}
-
 export async function day01() {
     const filepath = path.join(__dirname, "../inputs/day01.txt")
     const stream = fs.createReadStream(filepath);
@@ -25,11 +17,21 @@ export async function day01() {
     const numbers = Array.from({ length: 100 }, (_, i) => i);
     let dial = 50;
     let points = 0;
+    let extra = 0;
     for await (const line of rl) {
         const direction = line.substr(0, 1);
         const value = Number(line.substr(1));
-        dial = cycle(direction, dial, value);
+
+        for (let i = 0; i < value; i++) {
+            if (direction == "R") {
+                dial = (dial + 1) % 100;
+            } else {
+                dial = (dial - 1 + 100) % 100;
+            }
+            if (dial === 0) extra++;
+        }
         if (dial === 0) points++;
     }
     console.log("part one: ", points);
+    console.log("part two: ", extra);
 }
